4355955cafaf7f813c2926dd232ee4f9364e518d
CODELABSYS\Ahmed.ElBakrey <ahmed.elbakrey@codelabsys.com>
2025-09-09 08:54:57 +0300

    (master) -> initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from '../components/home-component/home-component';
 import { About } from '../components/about/about';
 import { Contact } from '../components/contact/contact';
+import { FirstComponent } from '../components/first-component/first-component';
+import { SecondComponent } from '../components/second-component/second-component';
+import { ThirdComponent } from '../components/third-component/third-component';
+import { FourthComponent } from '../components/fourth-component/fourth-component';
+
+
 
 export const routes: Routes = [
 
@@ -9,4 +15,9 @@ export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'about', component: About },
     { path: 'contact', component: Contact },
+    { path: 'first', component: FirstComponent },
+    { path: 'second', component: SecondComponent },
+    { path: 'third', component: ThirdComponent },
+    { path: 'fourth', component: FourthComponent }
+
  ];
